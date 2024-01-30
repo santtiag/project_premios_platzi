08d579a5999a487ff2999fff0ce45b067f3de8d3
Santiago <romerosantiago5002@gmail.com>
2024-01-30 16:52:18 -0500

    Creation of templates displaying the list of questions and through their url display the index of each one (without the answers yet)

--- premios_platzi_app/polls/urls.py
+++ premios_platzi_app/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from polls import views # it's the same as: from . import views
 
 urlpatterns = [ 
-    path('', views.index, name='index'),
+    path('', views.index),
     path('<int:question_id>/', views.detail, name='index'),
     path('<int:question_id>/result', views.result, name='index'),
     path('<int:question_id>/vote', views.vote, name='index'),
--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -1,13 +1,20 @@
+from django.shortcuts import render
+
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Esta en la página principal de Premios Platzi App')
+from polls.models import Question
 
+
+def index(request):
+    questions = Question.objects.all()
+    # return HttpResponse(latest_question_list)
+    return render(request, "index.html", {'questions': questions})
+    
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta número: {question_id}")
+    return HttpResponse("Estas viendo la pregunta número: %s" % question_id)
 
 def result(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta número: {question_id}")
+    return HttpResponse("Estas viendo los resultados de la pregunta número: %s" % question_id)
 
 def vote(request, question_id):
     return HttpResponse(f"Estas votando a la pregunta número: {question_id}")
