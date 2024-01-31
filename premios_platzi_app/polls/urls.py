6aff5200bbfc80c1b96a341566659670e62affd4
Santiago <romerosantiago5002@gmail.com>
2024-01-31 10:55:33 -0500

    Creation of the voting pages (functional), addition of the `Choice` table to the polls/admin.py file to view the data in django admin. Already modified the index, detail and vote views, missing resutl

--- premios_platzi_app/polls/urls.py
+++ premios_platzi_app/polls/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from polls import views # it's the same as: from . import views
 
+app_name = "polls"
+
 urlpatterns = [ 
-    path('', views.index),
-    path('<int:question_id>/', views.detail, name='index'),
-    path('<int:question_id>/result', views.result, name='index'),
-    path('<int:question_id>/vote', views.vote, name='index'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result', views.result, name='result'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
 
--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, render, reverse
 
 from django.http import HttpResponse
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 
 def index(request):
@@ -11,10 +11,22 @@ def index(request):
     return render(request, "index.html", {'questions': questions})
     
 def detail(request, question_id):
-    return HttpResponse("Estas viendo la pregunta número: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "detail.html", {'question':question})
 
 def result(request, question_id):
     return HttpResponse("Estas viendo los resultados de la pregunta número: %s" % question_id)
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta número: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.filter(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': 'No elegiste un respuesta',
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
