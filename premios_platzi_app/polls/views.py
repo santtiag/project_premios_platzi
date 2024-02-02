ce76b2f8948be28277ee70877176a95a7d03428c
Santiago <romerosantiago5002@gmail.com>
2024-02-02 13:58:35 -0500

    Fully functional voting functionality, addition of folders to follow the structure with layout and static, lastly addition of css to the templates

--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -7,22 +7,22 @@ from polls.models import Choice, Question
 
 def index(request):
     questions = Question.objects.all()
-    # return HttpResponse(latest_question_list)
-    return render(request, "index.html", {'questions': questions})
+    return render(request, "polls/index.html", { 'questions': questions })
     
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "detail.html", {'question':question})
+    return render(request, "polls/detail.html", { 'question':question })
 
 def result(request, question_id):
-    return HttpResponse("Estas viendo los resultados de la pregunta número: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', { 'question': question })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.filter(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': 'No elegiste un respuesta',
             })
